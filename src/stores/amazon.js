5cf3ef295033f30d2bf9af7bfdfaf0bbb90d7326
hilt <hackerhilt@gmail.com>
2020-12-09 18:43:33 -0300

    Add support to amazon

---
+++ src/stores/amazon.js
@@ -0,0 +1,35 @@
+module.exports = async ({
+    browser,
+    logger,
+    productURL,
+    productName: product,
+    messages,
+  }) => {
+    const log = logger.createContextLogger({
+      store: 'Amazon',
+      product,
+    });
+  
+    log.debug('Creating new page...');
+    const page = await browser.newPage();
+  
+    try {
+      log.debug(`Going to ${productURL}`);
+      await page.goto(productURL);
+  
+      const stockStatus = await page.evaluate(() => {
+        return (
+          document.querySelectorAll('#priceblock_ourprice').length >
+          0
+        );
+      });
+  
+      log.info(
+        stockStatus ? messages.IN_STOCK_MESSAGE : messages.OUT_OF_STOCK_MESSAGE
+      );
+      return stockStatus;
+    } catch (err) {
+      log.error(`Request failed =(`);
+    }
+  };
+  
