72620dfa233e8a7ff935e1e317048e437c541238
Jonathan <johnzballad@gmail.com>
2020-12-09 15:38:09 -0300

    hotfix: terabyte support with random user-agent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const pluginStealth = require('puppeteer-extra-plugin-stealth');
+
 const logger = require('./utils/logger');
 const scrape = require('./scrape');
+
+puppeteer.use(pluginStealth())
 
 const main = async () => {
   const products = [
@@ -16,18 +21,18 @@ const main = async () => {
       name: 'Asus RTX 3080',
       url: 'https://www.kabum.com.br/produto/121138/placa-de-v-deo-asus-nvidia-geforce-rtx3080-10gb-gddr6-tuf-rtx3080-10g-gaming'
     },
-    // {
-    //   name: 'MSI GT 710 2gb',
-    //   url: 'https://www.terabyteshop.com.br/produto/15568/placa-de-video-msi-geforce-gt-710-2gd3-lp-2gb-gddr3-64-bit-912-v809-2024'
-    // },
-    // {
-    //   name: 'Asus RTX 3080 TUF',
-    //   url: 'https://www.terabyteshop.com.br/produto/15107/placa-de-video-asus-tuf-geforce-rtx-3080-10gb-gddr6x-320bit-tuf-rtx3080-10g-gaming'
-    // }
+    {
+      name: 'MSI GT 710 2gb',
+      url: 'https://www.terabyteshop.com.br/produto/15568/placa-de-video-msi-geforce-gt-710-2gd3-lp-2gb-gddr3-64-bit-912-v809-2024'
+    },
+    {
+      name: 'Asus RTX 3080 TUF',
+      url: 'https://www.terabyteshop.com.br/produto/15107/placa-de-video-asus-tuf-geforce-rtx-3080-10gb-gddr6x-320bit-tuf-rtx3080-10g-gaming'
+    }
   ]
 
   logger.debug('Opening puppeteer browser...')
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
 
   await scrape({ browser, productList: products, logger });
 
--- src/stores/terabyte.js
+++ src/stores/terabyte.js
@@ -1,3 +1,5 @@
+const randomUseragent = require('random-useragent');
+
 module.exports = async ({
   browser, 
   logger,
@@ -10,19 +12,21 @@ module.exports = async ({
   log.debug('Creating new page...');
   const page = await browser.newPage();
 
+  log.debug('Getting a random user-agent');
+  const userAgent = await randomUseragent.getRandom();
+  
+  log.debug(`Setting user-agent to ${userAgent}`);
+  await page.setUserAgent(userAgent);
+  
   try {
     log.debug(`Going to ${productURL}`);
     await page.goto(productURL);
 
-    await page.screenshot({path: 'screenshot.png'});
-
-
     const stockStatus = await page.evaluate(() => {
+      return document.querySelectorAll('div#indisponivel').length <= 0;
+    })
 
-    return document.querySelectorAll('div#indisponivel').length > 0;
-  })
-
-    log.info(stockStatus ? messages.IN_STOCK_MESSAGE : messages.OUT_OF_STOCK_MESSAGE);
+    log.info(stockStatus ? messages.IN_STOCK_MESSAGE: messages.OUT_OF_STOCK_MESSAGE);
     return stockStatus;
   } catch (err) {
     log.error(`Request failed =(`)
